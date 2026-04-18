b3d1a591e63e8b7f71f8c468abfa672fd447c5cd
dongchan cho <dongchancho@dongchanui-MacBookPro.local>
2026-04-18 15:36:53 +0900

    init

---
+++ src/main/java/com/example/claude/ClaudeApplication.java
@@ -0,0 +1,13 @@
+package com.example.claude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudeApplication.class, args);
+	}
+
+}
